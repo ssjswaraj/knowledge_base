4d06f3bf43fa1c2f3bf5d15c0ffbf50217b4f777
Swaraj Gupta <37957790+ssjswaraj@users.noreply.github.com>
2026-08-25 08:34:03 +0530

    Add isAnagram method to validate anagrams
    Implement isAnagram method to check if two strings are anagrams.

---
+++ python_and_DSA/leetcode/242_Valid_Anagram.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        s_dict ={}
+        t_dict={}
+        if len(s)!=len(t):
+            return False
+        for i in s:
+            if i in s_dict:
+                s_dict[i]=s_dict[i]+1
+            else:
+                s_dict[i]=1
+        for i in t:
+            if i in t_dict:
+                t_dict[i]=t_dict[i]+1
+            else:
+                t_dict[i]=1
+        
+        for i in s_dict:
+
+            if i in t_dict:
+                if s_dict[i]!=t_dict[i]:
+                    return False
+            else:
+
+                return False
+        return True
